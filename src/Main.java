51cb1e68b20a3777784f96225a3f40f78427ff13
DASC <dasc@Mac-mini-de-DASC.local>
2026-09-10 11:55:58 -0700

    3 de 3

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,66 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 
 
 public class Main extends JPanel implements KeyListener {
 
-
+    //cuadro
     int x = 400;
     int y = 450;
 
     int velocidad = 11;
 
+    //cir
+    int circuloX;
+    int circuloY =0;
+
+    int velocidadCaida = 18;
+
+    Random random6 = new Random();
+
     public Main() {
         setFocusable(true);
         addKeyListener(this);
+        //aleaorio
+        circuloX = random6.nextInt(600);
     }
 
     @Override 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
         //e
-        g.setColor(Color.BLACK);
+
+        g.setColor(Color.decode("#000000"));
         g.fillRect(0, 0, getWidth(), getHeight());
+
+        // piso
+        g.setColor(Color.GRAY);
+        g.fillRect(0, 500, getWidth(), 100);
+
         //j
         g.setColor(Color.decode("#f71d78"));
         g.fillRect(x, y, 50, 50);
+
+        //c
+        g.setColor(Color.WHITE);
+        g.fillOval(circuloX, circuloY, 30, 30);
+    }
+
+    //m6ov5im6iento
+
+    public void actualizar() {
+        circuloY += velocidadCaida;
+
+        if(circuloY > 500) {
+            circuloY = 0;
+            circuloX = random6.nextInt(600);
+        }
+
+        repaint();
     }
 
     @Override
@@ -65,6 +100,15 @@ public class Main extends JPanel implements KeyListener {
         ventana.setVisible(true);
 
         juego.requestFocusInWindow();
+
+
+
+        Timer timer = new Timer(100, e -> {
+            juego.actualizar();
+           
+        });
+
+        timer.start();
     }
 
 }
